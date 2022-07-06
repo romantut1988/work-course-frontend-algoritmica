35506c46cbdb78a411f27aa695ee920a3e3f5b09
romantut1988 <romantut1988@mail.ru>
2022-07-06 11:39:57 +0300

    lesson 17 decision

--- lesson 17/js/script.js
+++ lesson 17/js/script.js
@@ -1,11 +1,25 @@
 // 1. Презентация. Элементы на веб-странице.
 
 // 2. Упражнение 1. Вывод элемента.
+const element = document.querySelector("li");
+console.log(element);
 
 // 3. Упражнение 2. Изменение списка.
+const element = document.querySelector('h1');
+element.remove("h1");
+
+// let newElement = document.createElement('li');
+const li = document.querySelector("li")
+li.append('TITAN RTX')
+
 
 // 4. Упражнение 3. Изменение элементов.
+const element = document.querySelector('h1');
+element.innerHTML = 'GeForce 20 Series';
 
-// 5. Задача. Список учеников  
+const colorElement = document.querySelector('.primary');
+colorElement.style.color = '#ababab';
+
+// 5. Задача. Список учеников
 
 // 6. Задача. Генерация разметки.
