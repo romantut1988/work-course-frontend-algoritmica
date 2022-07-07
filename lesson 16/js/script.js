e7b3add7c0aadc2e2d41e539ec63f76caaf0d8c4
romantut1988 <romantut1988@mail.ru>
2022-07-07 20:59:25 +0300

    lesson 16 decision

--- lesson 16/js/script.js
+++ lesson 16/js/script.js
@@ -1,16 +1,33 @@
 // 1. Презентация. Работа со строками.
 
 // 2. Упражнение. Имя и фамилия.
+let text = "'Вас зовут";
+let firstName = 'Alan';
+let lastName = 'Turing';
+let fullName = text + ' ' + firstName + ' ' + lastName + '...';
+console.log(fullName);
 
 // 3. Упражнение. Шаблонная строка.
+let firstName = 'Alan';
+let lastName = 'Turing';
+let greet = `“Рады снова видеть вас, ${firstName} ${lastName}`;
+console.log(greet);
 
 // 4. Упражнение. Количество букв.
+let fullName = "Alan Turing";
+console.log(fullName.length - 1);
 
-// 5. Упражнение. Ввод имени.  
+// 5. Упражнение. Ввод имени.
+let value = prompt('Привет, введи своё имя?');
+console.log(`Привет ${value}`);
 
 // 6. Упражнение. Операции над числами.
 
 // 7. Упражнение. Операции над числами 2.
+const num = String(12345 + "" + 98765);
+console.log(num[5]);
+
+// 8. Программу для вывода суммы цифр.
 
 // 9. Изменение порядка.
 
