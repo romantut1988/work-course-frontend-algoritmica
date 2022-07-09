216ee94fff5d4cd54f2c122cf67bc925a8edae8b
romantut1988 <romantut1988@mail.ru>
2022-07-09 21:52:50 +0300

    23 lesson solution

--- lesson 23/js/script.js
+++ lesson 23/js/script.js
@@ -1,12 +1,28 @@
 // 1. Презентация. Объекты.
 
 // 2. Упражнение. Свойство объекта.
+const user = {
+    name: 'Eric Cartman',
+    gender: 'male',
+    birthday: '07.08.2002'
+};
+console.log(user.birthday);
 
 // 3. Упражнение. Добавление и удаление.
+const user = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthDate: '1960-05-14',
+    department: 'management',
+    isActive: true,
+}
 
+user.age = 32
+delete user.birthDate;
+console.log(user);
 // 4. Упражнение. Методы объекта.
 
-// 5. Задача 1. Копия товара. 
+// 5. Задача 1. Копия товара.
 
 // 6. Задача. Калькулятор.
 
