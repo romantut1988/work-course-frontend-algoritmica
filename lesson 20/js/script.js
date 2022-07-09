500af379c6ac1d878bf0b7fb4085bbf9ef9ef19e
romantut1988 <romantut1988@mail.ru>
2022-07-09 16:09:18 +0300

    20 lesson solving part of the problems

--- lesson 20/js/script.js
+++ lesson 20/js/script.js
@@ -3,15 +3,27 @@
 // 2. Упражнение. Функция logGreeting().
 
 function logGreeting() {
-    console.log("Roman is a JS developer");
+    console.log(`${name} is a JS developer `);
 }
+let name = "Roman";
 logGreeting();
 
 // 3.  Упражнение. Функция с параметром.
+function showMessage(name) {
+    console.log(`${name} is active now`);
+}
+
+showMessage('Roman');
+showMessage('Valera');
+showMessage('Victoria');
 
 // 4. Упражнение. Функция для определения наибольшего.
 
-// 5. Упражнение. Наименьшее число. 
+// 5. Упражнение. Наименьшее число.
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+console.log(min(7, 15));
 
 // 6. Задача. Баланс.
 
