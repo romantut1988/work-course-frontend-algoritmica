f787b9896cc2a9dda657db374599ce83866ddc18
romantut1988 <romantut1988@mail.ru>
2022-07-09 23:55:46 +0300

    23 lesson adding

--- lesson 24/js/script.js
+++ lesson 24/js/script.js
@@ -1,8 +1,19 @@
 // 1. Презентация. Коллекции элементов.
 
 // 2. Упражнение 1. Вывод коллекции элементов.
+const elements = document.querySelectorAll('.primary');
+elements.forEach((elements) => {
+    console.log(elements);
+});
 
 // 3. Упражнение 2. Изменение классов.
+const activeUl = document.querySelector('ul');
+activeUl.classList.add('item');
+console.log(activeUl);
+
+const activeLi = document.querySelector('li');
+activeLi.classList.add('secondary');
+console.log(activeLi);
 
 // 4. Задача. Назначение стилей.
 
