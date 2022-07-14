566c397b41d0c7ebad84d85e57d2295cd07d0e57
romantut1988 <romantut1988@mail.ru>
2022-07-14 12:26:05 +0300

    lesson 17 adding problem solutions

--- lesson 17/js/script.js
+++ lesson 17/js/script.js
@@ -21,5 +21,32 @@ const colorElement = document.querySelector('.primary');
 colorElement.style.color = '#ababab';
 
 // 5. Задача. Список учеников
+let list = document.querySelector('ul');// список учеников 
+
+let element1 = document.createElement('li'); // элемент списка
+element1.innerHTML = 'Валера Миськевич';
+list.append(element1);
+
+let element2 = document.createElement('li');
+element2.innerHTML = 'Вика Капустина';
+list.prepend(element2);
+
+
+let element3 = document.createElement('li');
+element3.innerHTML = 'Попов Даниил';
+list.append(element3);
+
+let element4 = document.createElement('li');
+element4.innerHTML = 'Рашевский Алексей';
+list.append(element4);
+
+
+list.style.color = 'blue';
+
+let student = prompt('Введите имя ученика');
+let element5 = document.createElement('li');
+element5.innerHTML = student;
+list.append(element5);
+element5.style.color = 'red';
 
 // 6. Задача. Генерация разметки.
