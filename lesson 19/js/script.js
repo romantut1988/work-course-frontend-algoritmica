30bd3d190da676ec913f5e72bb6a8a26dc28074b
romantut1988 <romantut1988@mail.ru>
2022-07-18 21:04:44 +0300

    lesson 19 task condition

--- lesson 19/js/script.js
+++ lesson 19/js/script.js
@@ -1,1 +1,10 @@
 // 1. Практическое задание Анкета.
+let nameUser = prompt("name?"),
+    let firstName = prompt("firstName?"),
+    age = +prompt('age?', 18);
+city = promte("city");
+mail = prompt("mail")
+console.log(nameUser);
+console.log(firstName);
+console.log(age);
+console.log(mail);
