2ec67ec5af64f45ab2c93cb6be2bc5a815fd3617
romantut1988 <romantut1988@mail.ru>
2022-07-20 22:05:21 +0300

    lesson 24 tasks

--- lesson 24/js/script.js
+++ lesson 24/js/script.js
@@ -18,3 +18,20 @@ console.log(activeLi);
 // 4. Задача. Назначение стилей.
 
 // 5. Задача. Таблица криптовалют. 
+const data = [
+    { "name": "Биткоин", "ticker": "BTC", "value": "9685", "change": "2.83%" },
+    { "name": "Эфириум", "ticker": "ETH", "value": "210.5", "change": "6.17%" },
+    { "name": "Рипл", "ticker": "XRP", "value": "0.2812", "change": "2.47%" },
+    { "name": "Bitcoin Cash", "ticker": "BCH", "value": "441.4", "change": "5.01%" },
+    { "name": "Bitcoin SV", "ticker": "BSV", "value": "303.17", "change": "5.53%" },
+    { "name": "Лайткоин", "ticker": "LTC", "value": "74.935", "change": "4.25%" },
+    { "name": "Tether", "ticker": "USDT", "value": "0.9994", "change": "-1.7%" },
+    { "name": "EOS", "ticker": "EOS", "value": "4.6161", "change": "3.15%" },
+    { "name": "Binance Coin", "ticker": "BNB", "value": "19.824", "change": "-3.82%" },
+    { "name": "Cardano", "ticker": "ADA", "value": "0.060389", "change": "2.93%" },
+    { "name": "Tezos", "ticker": "XTZ", "value": "2.1247", "change": "6.12%" },
+    { "name": "Эфириум Классик", "ticker": "ETC", "value": "12.5988", "change": "4.09%" },
+    { "name": "Stellar", "ticker": "XLM", "value": "0.07034", "change": "-4.10%" },
+    { "name": "Monero", "ticker": "XMR", "value": "79.523", "change": "3.45%" },
+    { "name": "TRON", "ticker": "TRX", "value": "0.020881", "change": "5.21%" }
+];
