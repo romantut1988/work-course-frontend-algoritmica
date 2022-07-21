ce295a22eaceb6872eb8ca1ab32dc8cfa2c5a5fe
romantut1988 <romantut1988@mail.ru>
2022-07-21 21:14:48 +0300

    lesson 19 project decision and editing

--- lesson 19/js/script.js
+++ lesson 19/js/script.js
@@ -11,24 +11,25 @@
 
 
 let elementName = document.querySelector('.name');
-let name = window.prompt('Введите свое имя:');
+let name = prompt('Введите свое имя:');
 elementName.innerText = 'Имя: ' + name;
 
 
 elementSurname = document.querySelector('.surname');
-let surname = window.prompt('Введите свою фамилию:');
+let surname = prompt('Введите свою фамилию:');
 elementSurname.innerText = 'Фамилия: ' + surname;
 
 
 elementAge = document.querySelector('.age');
-let age = window.prompt('Введите свой возраст:');
+let age = prompt('Введите свой возраст:');
 elementAge.innerText = 'Возраст: ' + age;
 
 
 elementCity = document.querySelector('.city');
-let city = window.prompt('Введите свой город:');
+let city = prompt('Введите свой город:');
 elementCity.innerText = 'Город: ' + city;
 
 elementLink = document.querySelector('.link');
-let link = window.prompt('Вставьте ссылку на свою соцсеть:');
+let link = prompt('Вставьте ссылку на свою соцсеть:');
 elementLink.href = link;
+
