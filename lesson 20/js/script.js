0980670e48e7db353422d2d911491ea6b0d82d19
romantut1988 <romantut1988@mail.ru>
2022-07-28 12:09:12 +0300

    lesson 20 problem solving

--- lesson 20/js/script.js
+++ lesson 20/js/script.js
@@ -47,8 +47,48 @@ function min(a, b) {
 console.log(min(7, 15));
 
 // 6. Задача. Баланс.
+function logConvertor(dolars, rubles, curency) {
+    let money = Number(rubles) + Number(dolars * curency);
+    console.log('Вся сумма в рублях: ' + money);
+}
+let usersDolars = prompt('Введите сумму в доларах:');
+let curency = 2.54;
+let usersRubles = prompt('Введите сумму в рублях:');
+logConvertor(usersDolars, usersRubles, curency);
 
 // 7. Задача. Функция calc.
+function calc(num1, num2, operation) {
+
+    if (operation == '+') {
+        let num3 = num1 + num2;
+        return num3;
+    }
+
+    if (operation == '-') {
+        let num3 = num1 - num2;
+        return num3;
+    }
+
+    if (operation == '*') {
+        let num3 = num1 * num2;
+        return num3;
+    }
+
+    if (operation == '/') {
+        let num3 = num1 / num2;
+        return num3;
+    }
+
+}
+
+let user_num1 = +prompt('Введите 1 число')
+let user_num2 = +prompt('Введите 2 число')
+let user_operation = prompt('Введите знак')
+
+num3 = document.createElement('p');
+num3.innerHTML = "Значение:" + ' ' + calc(user_num1, user_num2, user_operation);
+document.body.append(num3);
+
 
 // 8. Задача. Функция isLucky.
 
