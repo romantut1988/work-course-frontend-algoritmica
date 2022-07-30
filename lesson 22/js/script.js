ea9f462008a235bd72fb24b232a4103252e9c49e
romantut1988 <romantut1988@mail.ru>
2022-07-30 22:46:36 +0300

    lesson 22 problem solving

--- lesson 22/js/script.js
+++ lesson 22/js/script.js
@@ -1,12 +1,30 @@
 // 1. Презентация. Массивы.
 
 // 2. Упражнение. Изменение значения элемента.
+const names = ['Victor', 'Gwen', 'Katrin'];
+console.log(names[1]);
+names[1] = 'Classified';
+console.log(names);
 
 // 3. Упражнение. I know how to use cycles.
+const classmates = ['Peter', 'Gwen', 'Norman'];
+classmates.push('Vasia');
+classmates.shift();
+console.log(classmates);
 
 // 4. Упражнение. Все отриц числа.
+function oddNumbers(array) {
+    for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+        if (item % 2)
+            console.log(item);
+    }
+}
 
-// 5. Упражнение. Все положит числа. 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+oddNumbers(numbers);
+
+// 5. Упражнение. Все положит числа.
 
 // 6. Задача. Массив фильмов.
 
@@ -14,4 +32,3 @@
 
 // 8. Kebab-case в UPPER_CASE.
 
-  
