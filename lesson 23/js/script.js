c6cc2fba84ca035430c4b9bdbce8bd50d1d52423
romantut1988 <romantut1988@mail.ru>
2022-08-03 19:58:56 +0300

    lesson 23 decision

--- lesson 23/js/script.js
+++ lesson 23/js/script.js
@@ -25,5 +25,23 @@ const user = {
 // 5. Задача 1. Копия товара.
 
 // 6. Задача. Калькулятор.
-
+let calculator = {
+    sum: function() {
+      return this.a + this.b;
+    },
+  
+    mul: function() {
+      return this.a * this.b;
+    },
+  
+    readValues: function() {
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    }
+  }
+  
+  calculator.readValues();
+  alert(calculator.sum());
+  alert(calculator.mul());
+  
 // 7. Задача. Клиент с кредитной картой.
