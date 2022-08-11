c5d04c5d0006684362d22761c51d6eab6843cb28
romantut1988 <romantut1988@mail.ru>
2022-08-11 12:07:25 +0300

    lesson 25 solution

--- lesson 25/js/script.js
+++ lesson 25/js/script.js
@@ -1,15 +1,34 @@
 // 1. Презентация. Обработка событий. 
 
 // 2. Упражнение 1. Добавление обработчика событий.
-const buttons = document.querySelectorAll('.action');
+// const elements = document.querySelectorAll('button');
+// function changeColor() {
+//     this.style.color = 'grey'
+// }
+// elements.forEach((element) => {  
+//   element.addEventListener('click', changeColor);
+// });
+
+const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', function () {
-        button.style.color = 'red';
+        button.style.color = 'grey';
     })
 })
 // 3. Упражнение 2. Удаление обработчика событий.
+const elements = document.querySelectorAll('button');
+function changeText() {
+    this.classList.add("active");
+    event.target.removeEventListener('click', changeText);
+}
+elements.forEach((element) => {
+    element.addEventListener('click', changeText);
+});
 
 // 4. Упражнение 3. Добавление событий клавиатуры.
+document.addEventListener('keyup', event => {
+    if (event.code === 'Enter') console.log('enter');
+});
 
 // 5. Задача 1. Карточка товара с доп. информацией.
 
