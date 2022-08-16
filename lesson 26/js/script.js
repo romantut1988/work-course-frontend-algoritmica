35aadfad95a0a126b7bf8ef7c2755dc794f25058
romantut1988 <romantut1988@mail.ru>
2022-08-16 13:51:57 +0300

    lesson 26 problem solving

--- lesson 26/js/script.js
+++ lesson 26/js/script.js
@@ -3,7 +3,14 @@
 // 2. Упражнение 1. Создание формы.
 
 // 3. Упражнение 2. Получение значений.
-
+let name = document.querySelector('.name-input');
+let comment = document.querySelector('.comment-input');
+let send = document.querySelector('button');
+function printValue() {
+    console.log(name.value)
+    console.log(comment.value)
+}
+send.addEventListener('click', printValue)
 // 4. Задача. Конвертер валют.
- 
+
 
