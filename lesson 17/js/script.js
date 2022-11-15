cabddf5b5d6b7c969a3e018ed8a84406e8b0e1aa
romantut1988 <romantut1988@mail.ru>
2022-11-15 14:09:22 +0300

    lesson 17 problem solving

--- lesson 17/js/script.js
+++ lesson 17/js/script.js
@@ -53,3 +53,41 @@ list.append(element5);
 element5.style.color = 'red';
 
 // 6. Задача. Генерация разметки.
+const list = document.querySelector('ul');
+let s1 = document.createElement('li');
+s1.innerHTML = "Albert Einstein. Theoretical physicist, widely acknowledged to be one of the greatest physicists of all time.";
+s1.style.color = 'red';
+s1.style.fontStyle = 'italic';
+s1.style.margin = '10px';
+s1.style.listStyle = 'none';
+list.append(s1);
+let s2 = document.createElement('li');
+s2.innerHTML = "Thomas Edison. American inventor and businessman.";
+s2.style.color = 'red';
+s2.style.fontStyle = 'italic';
+s2.style.margin = '10px';
+s2.style.listStyle = 'none';
+list.append(s2);
+let s3 = document.createElement('li');
+s3.innerHTML = "Nikola Tesla. Serbian-American inventor, electrical engineer, mechanical engineer, and futurist.";
+s3.style.color = 'red';
+s3.style.fontStyle = 'italic';
+s3.style.margin = '10px';
+s3.style.listStyle = 'none';
+list.append(s3);
+
+const body = document.querySelector('body');
+let form = document.createElement('form');
+body.append(form);
+
+let in1 = document.createElement('input');
+in1.value = "Name";
+form.append(in1);
+
+let in2 = document.createElement('input');
+in2.value = "Surname";
+form.append(in2);
+
+let press = document.createElement('button');
+press.innerText = "Submit";
+form.append(press);
